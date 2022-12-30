044a93fee1dad0cb8f7ce10f9ed0b48ec53b7bf7
Ana <ana.pinheiro@telecall.com>
2022-12-30 11:41:50 -0300

    desafio entregue

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string ime, int memoria) : base(numero, modelo, ime, memoria)
+        {
+
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o {nomeApp} no iphone");
+        } 
+    
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iphone: ");
+Smartphone iphone = new Iphone(numero:"21 2225-5525", modelo: "Iphone",ime: "23",memoria: 32);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Ba");
+
+
+Console.WriteLine("Nokia: ");
+Smartphone nokia = new Nokia("21 9205-2825", "Nokia", "3", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Ba");
